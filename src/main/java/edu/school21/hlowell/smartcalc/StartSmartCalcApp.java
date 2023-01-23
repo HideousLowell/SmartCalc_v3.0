3245ec1712e4afba1e46301171dbb1b00a7ec2d0
hlowell <email>
2023-01-23 17:16:01 +0700

    Delete HistoryModel.java

--- src/main/java/edu/school21/hlowell/smartcalc/StartSmartCalcApp.java
+++
@@ -1,9 +0,0 @@
-package edu.school21.hlowell.smartcalc;
-
-import javafx.application.Application;
-
-public class StartSmartCalcApp {
-    public static void main(String[] args) {
-        Application.launch(SmartCalcApp.class);
-    }
-}
